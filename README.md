50f3b4cfa81dfe8c0c44f945ffffd8cfdd5d595e
YinQY <qiyao.yin@hotmail.com>
2022-10-11 11:54:30 +0800

    add files

--- README.md
+++
@@ -1,6 +0,0 @@
-# RL-for-DCVRP
-data: the training and test data.
-
-test: the test code.
-
-train: the training code.
--- train/device.py
+++
@@ -1,5 +0,0 @@
-import torch
-
-print(torch.cuda.is_available())
-device = torch.device("cuda:3" if torch.cuda.is_available() else "cpu")
-print(device)
